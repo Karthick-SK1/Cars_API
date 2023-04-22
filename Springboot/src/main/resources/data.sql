6da239d6d182db21b2b7dbf57964f7dd08f07e14
Karthick Sha <karthickshan766@gmail.com>
2023-04-22 12:43:23 +0530

    Replaced MYSQL Database with H2 Database

---
+++ Springboot/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Car_bean (id int , brand varchar , price varchar, model varchar);
+insert into Car_bean values ( 1,'Hyundai','12,00,000','i20' );
+insert into Car_bean values ( 2,'Volkswagen','14,00,000','Polo GT' );
+insert into Car_bean values ( 3,'Tata','10,00,000','Altroz' );
+insert into Car_bean values ( 4,'Suzuki','09,00,000','Swift' );
+insert into Car_bean values ( 5,'Audi','1.20 C','R8' );
